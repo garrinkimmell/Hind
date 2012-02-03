32ad630e105f8aa4b963e6ed3b8df3a4b3d1b3b2
Garrin Kimmell <garrin.kimmell@gmail.com>
2012-02-02 23:30:17 -0600

    Removed some dead code.

--- Hind/PathCompression.hs
+++ Hind/PathCompression.hs
@@ -24,7 +24,7 @@ stateCharacteristic k =
              Term_spec_constant (Spec_constant_numeral k)]]))
 
 
-
+-- | Generate the signature for path compression.
 pathCompressionSig :: HindFile -> [Command]
 pathCompressionSig hf = decls ++ defines
   where decls =
@@ -41,41 +41,6 @@ pathCompressionSig hf = decls ++ defines
              [(Term_qual_identifier_ (Qual_identifier v) [idx]) | (_,vars) <- hindStates hf, v <- vars]
 
             ])]
-
-        idx = Term_qual_identifier (Qual_identifier (Identifier idxName))
-        idxName = "___idx"
-
--- | Generate the signature for path compression.
--- pathCompressionSig :: HindFile -> [Command]
-pathCompressionSig hf = decls  ++ defines ++ top
-  where decls =
-          [Declare_fun (pcname sort )
-                        (replicate (length ids) sort) (Sort_identifier (Identifier "Int"))
-           | (sort,ids) <- hindStates hf]
-        defines =
-          [Define_fun (pcname sort ++ "_n")
-                        [(Sorted_var idxName (Sort_identifier (Identifier "Int")))]
-                          Sort_bool
-                         (Term_qual_identifier_ (Qual_identifier (Identifier "="))
-                          [(Term_qual_identifier_ (Qual_identifier (Identifier (pcname sort)))
-                            [Term_qual_identifier_ (Qual_identifier i)
-                               [idx]
-                                 | i <- ids]),
-                           idx])
-
-           | (sort,ids) <- hindStates hf]
-        top = [Define_fun ("___path_compression")
-               [Sorted_var "___idx" (Sort_identifier (Identifier "Int"))] Sort_bool
-               (Term_qual_identifier_ (Qual_identifier (Identifier "and"))
-                [Term_qual_identifier_ (Qual_identifier (Identifier (pcname sort ++ "_n"))) [idx]
-                | (sort,_) <- hindStates hf])]
-
-
-
-        pcname sort = "___path_compression_" ++ pcSort sort
-
-        pcSort (Sort_bitvec n) = "bitvec_" ++ show n
-        pcSort s = show s
 
         idx = Term_qual_identifier (Qual_identifier (Identifier idxName))
         idxName = "___idx"
